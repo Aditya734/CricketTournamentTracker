f0afe5ad72c73aca02f282da4aeb6e42c216c164
Aditya Chouksey <aditya.chouksey734@gmail.com>
2019-05-25 21:08:16 +0530

    TextConnection completed for PrizeForm

---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+//Load the text file
+//Convert the text to List<Prizes>
+//Find the Max ID
+//Add the new record with new ID(Max+1)
+//Convert the prize to List<string>
+//Save the string to the text file
+
+namespace TrackerLibrary.DataAccess.TextHelper
+{
+    public static class TextConnectorProcessor
+    {
+        public static string FullFilePath(this string fileName) //extension method of string class.
+        {
+            return $"{ConfigurationManager.AppSettings["FilePath"]}\\{fileName}"; 
+            //will take the name from app.config where tag is <AppSetting> and Key = "FilePath"
+        }
+
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModel(this List<string> lines)
+        {
+            List<PrizeModel> OutPut = new List<PrizeModel>();
+
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split(',');
+                
+
+                PrizeModel P = new PrizeModel
+                {
+                    Id = int.Parse(cols[0]),
+                    PlaceNumber = int.Parse(cols[1]),
+                    PlaceName = cols[2],
+                    PrizeAmount = decimal.Parse(cols[3]),
+                    PrizePercentage = double.Parse(cols[4])
+                };
+                OutPut.Add(P);
+            }
+            return OutPut;
+        }
+
+        public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (PrizeModel p in models)
+            {
+                lines.Add($"{ p.Id },{ p.PlaceNumber },{ p.PlaceName },{ p.PrizeAmount },{ p.PrizePercentage }");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+        }
+    }
+}
